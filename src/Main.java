29e51085c50c9766f8c0495df477beff64cabfb9
AndreevSV <51971450+AndreevSV@users.noreply.github.com>
2022-10-05 22:11:25 +0300

    Homework done.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        //task1();
-        //task2();
-        //task3();
-        //task4();
+        task1();
+        task2();
+        task3();
+        task4();
         task5();
     }
     public static void task1() {
